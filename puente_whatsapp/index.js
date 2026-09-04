d09f2564551432b509ef50eaee5e5727d1109a30
Santiago Iturralde <siturralde@alumnos.exa.unicen.edu.ar>
2026-09-04 20:03:24 -0300

    Fix dependencias npm estables y endpoints del puente

--- puente_whatsapp/index.js
+++ puente_whatsapp/index.js
@@ -7,6 +7,19 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+app.use(express.json());
+
+// Permitir peticiones desde la app Flutter Web (CORS)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 const PORT = process.env.PORT || 10000;
 
 let qrCodeData = null;
